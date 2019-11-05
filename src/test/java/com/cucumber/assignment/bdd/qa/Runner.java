8053894a009a253406a7353d9422113595e3c797
AmeenaM <ameenamoghal23@gmail.com>
2019-11-05 22:07:24 +0000

    Changes done in Runner class

--- src/test/java/com/cucumber/assignment/bdd/qa/Runner.java
+++ src/test/java/com/cucumber/assignment/bdd/qa/Runner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources", dryRun = false, strict=true, tags="@booking")
+@CucumberOptions(features="src/test/resources", dryRun = false, strict=true, tags="@booking",plugin = {"json:target/Ameena.json","pretty"} )
 public class Runner
 {
 
